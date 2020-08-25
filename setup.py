9fd9cad4854bba680672a389a5f6c275a2bbfdd8
90houlaoheshang <907333918@qq.com>
2020-08-25 12:28:17 +0800

    完善模块配置

--- setup.py
+++ setup.py
@@ -9,12 +9,14 @@ setuptools.setup(
     author="Antas",
     author_email="",
     description='''
-Think about such scene, some object has different state or periods, well we call periods.Among these periods, there 
+    Think about such scene, some object has different state or periods, well we call periods.Among these periods, there 
 can't be any two that could be both the present period. The object could be in only one period. For example, a human can 
 only be one period of baby, youth, adult, old man and dead.
 
 So now not only can you get the exact period, but also await inside the period, await outside the period, await time when 
-the object enters the period and await time when the object exits the period.          
+the object enters the period and await time when the object exits the period.
+
+Remember to distribute different periods for one object.          
     ''',
     long_description=long_description,
     long_description_content_type="text/markdown",
