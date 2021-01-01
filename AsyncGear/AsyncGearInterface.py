6d334d04bd10880a6f4bd92cbc83f9f7e76220d2
90houlaoheshang <907333918@qq.com>
2021-01-01 21:39:26 +0800

    由于Python目前不支持，删除Gear接口里的run_when装饰器

--- AsyncGear/AsyncGearInterface.py
+++ AsyncGear/AsyncGearInterface.py
@@ -5,8 +5,6 @@ import asyncio
 
 from .AsyncGear import AsyncGear
 
-from .run_when import run_when_inside, run_when_exit, run_when_enter, run_when_outside
-
 
 class Gear:
 
@@ -43,14 +41,14 @@ class Gear:
     async def wait_exit_period(self, period_name: str):
         return await asyncio.create_task(AsyncGear.wait_exit_period(self.obj, period_name))
 
-    def when_enter(self, period_name: str, queue_blocking='abandon'):
-        return run_when_enter(self.obj, period_name, queue_blocking)
-
-    def when_exit(self, period_name: str, queue_blocking='abandon'):
-        return run_when_exit(self.obj, period_name, queue_blocking)
-
-    def when_inside(self, period_name: str, queue_blocking='abandon'):
-        return run_when_inside(self.obj, period_name, queue_blocking)
-
-    def when_outside(self, period_name: str, queue_blocking='abandon'):
-        return run_when_outside(self.obj, period_name, queue_blocking)
+    # def when_enter(self, period_name: str, queue_blocking='abandon'):
+    #     return run_when_enter(self.obj, period_name, queue_blocking)
+    #
+    # def when_exit(self, period_name: str, queue_blocking='abandon'):
+    #     return run_when_exit(self.obj, period_name, queue_blocking)
+    #
+    # def when_inside(self, period_name: str, queue_blocking='abandon'):
+    #     return run_when_inside(self.obj, period_name, queue_blocking)
+    #
+    # def when_outside(self, period_name: str, queue_blocking='abandon'):
+    #     return run_when_outside(self.obj, period_name, queue_blocking)
