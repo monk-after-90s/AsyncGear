8ccd50d10e3e68b21ae908b6c29732dc9bd1f99d
90houlaoheshang <907333918@qq.com>
2021-01-01 21:45:05 +0800

    开放run_when装饰器接口

--- AsyncGear/__init__.py
+++ AsyncGear/__init__.py
@@ -1,3 +1,4 @@
 from .AsyncGearInterface import Gear
+from .run_when import run_when_inside, run_when_exit, run_when_enter, run_when_outside
 
-__all__ = ['Gear']
+__all__ = [Gear, run_when_inside, run_when_exit, run_when_enter, run_when_outside]
