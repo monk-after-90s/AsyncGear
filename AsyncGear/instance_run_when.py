87860b0179ccebe07d05638cea07e5c0dd0f28d5
90houlaoheshang <907333918@qq.com>
2021-01-05 14:20:01 +0800

    添加文档

--- AsyncGear/instance_run_when.py
+++ AsyncGear/instance_run_when.py
@@ -24,17 +24,51 @@ def _when_ins(time_method: str, period_name: str, queue_blocking='abandon'):
     return decorator
 
 
-def when_enter(period_name: str, queue_blocking='abandon'):  # todo 文档
+def when_enter(period_name: str, queue_blocking='abandon'):
+    '''
+    Decorate a class method or its instance method, to be run when the class or the instance gear enters period
+    period_name. Remember to use in a coroutine and set the gear periods.
+
+    :param period_name:
+    :param queue_blocking: When the decorated is activated too frequently, 'non_block' means run immediately anyway; 'queue' means
+                             waits the previous one completing then run the new activated; 'abandon' means abandon the new
+                             activated if the previous one has not completed yet.
+    :return:
+    '''
     return _when_ins('enter', period_name, queue_blocking)
 
 
 def when_exit(period_name: str, queue_blocking='abandon'):
+    '''
+    Decorate a class method or its instance method, to be run when the class or the instance gear exits period
+    period_name. Remember to use in a coroutine and set the gear periods.
+
+    :param period_name:
+    :param queue_blocking: When the decorated is activated too frequently, 'non_block' means run immediately anyway; 'queue' means
+                             waits the previous one completing then run the new activated; 'abandon' means abandon the new
+                             activated if the previous one has not completed yet.
+    :return:
+    '''
     return _when_ins('exit', period_name, queue_blocking)
 
 
 def when_inside(period_name: str, ):
+    '''
+    Decorate a class method or its instance method, to be run when the class or the instance gear is inside period
+    period_name. Remember to use in a coroutine and set the gear periods.
+
+    :param period_name:
+    :return:
+    '''
     return _when_ins('inside', period_name, 'abandon')
 
 
 def when_outside(period_name: str, ):
+    '''
+    Decorate a class method or its instance method, to be run when the class or the instance gear is outside period
+    period_name. Remember to use in a coroutine and set the gear periods.
+
+    :param period_name:
+    :return:
+    '''
     return _when_ins('outside', period_name, 'abandon')
